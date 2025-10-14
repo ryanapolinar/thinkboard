af5f2801b7c63b2ac8f358b81919dff20770ea8b
Ryan Apolinar <apolinaryan@gmail.com>
2025-10-14 10:48:03 -0700

    Prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv"
+import path from "path";
 
 // Routes
 import notesRoutes from "./routes/notesRoutes.js";
@@ -15,6 +16,7 @@ dotenv.config();
 // Connect to database
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 /* MIDDLEWARE
 * After sending request, server sends response.
@@ -22,9 +24,14 @@ const PORT = process.env.PORT || 5001;
 *
 * Popular use cases: auth check, rate limiting (Upstash Redis)
 */
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
+
+// Use localhost when developing locally
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }));
+}
+
 app.use(express.json()); // middleware parses JSON bodies: gives us access to request.body
 app.use(rateLimiter);
 
@@ -37,6 +44,16 @@ app.use(rateLimiter);
 
 // Routes
 app.use("/api/notes", notesRoutes)
+
+// When on production, handle the frontend at the same location as the backend
+if (process.env.NODE_ENV === "production") {
+    // Serve the frontend react application as a static asset under the backend.
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (request, response) => {
+        response.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+})
+}
 
 // Start the server AFTER we connect to the database
 connectDB().then(() => {
